a7fccdccbe2689327647a5d327167dfc0143b424
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-19 15:23:08 +0530

    Update passwordGenerator.js

--- password-generator/passwordGenerator.js
+++ password-generator/passwordGenerator.js
@@ -1,1 +1,14 @@
+function generatePassword(length) {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let password = '';
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    
+    return password;
+}
+
+
 
