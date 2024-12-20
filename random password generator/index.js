08d43a73572c3484cb7a275d4f08885bad9c45c6
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-20 18:01:54 +0530

    Update index.js

--- random password generator/index.js
+++ random password generator/index.js
@@ -3,7 +3,9 @@
 function getRandomJoke() {
     const jokes = [
         { setup: "Why don't skeletons fight each other?", punchline: "They don't have the guts." },
-       
+        { setup: "Why did the math book look sad?", punchline: "Because it had too many problems." },
+        { setup: "What do you call fake spaghetti?", punchline: "An impasta!" },
+        { setup: "How do you organize a space party?", punchline: "You planet." },
         { setup: "Why don't some couples go to the gym?", punchline: "Because some relationships don't work out." }
     ];
 
