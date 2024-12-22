fc5d33f2f6652d070aa87eea87c61b34486f4233
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-22 19:25:24 +0530

    Update index.js

--- calculator /index.js
+++ calculator /index.js
@@ -1,1 +1,21 @@
+// simpleCalculator.js
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Error: Division by zero is not allowed.";
+    }
+    return a / b;
+}
 
