0d1e6d8177129cd52d5fc27995bd2fb386b4f57c
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-24 17:05:29 +0530

    Update index.js

--- Number Guessing Game/index.js
+++ Number Guessing Game/index.js
@@ -6,6 +6,10 @@ function numberGuessingGame() {
     let attempts = 0;
     let guessedCorrectly = false;
 
+    console.log("Welcome to the Number Guessing Game!");
+    console.log("Guess a number between 1 and 100.");
+
+   
     
 }
 
