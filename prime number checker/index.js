73e0f4ce6bc0b2aecf428bce915c204b9a1d66f9
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-25 20:37:02 +0530

    Update index.js

--- prime number checker/index.js
+++ prime number checker/index.js
@@ -1,1 +1,15 @@
 
+// primeChecker.js
+
+function isPrime(number) {
+    if (number <= 1) return false; // Numbers less than 2 are not prime
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false; // Divisible by a number other than 1 and itself
+    }
+    return true;
+}
+
+
+// Run the Prime Checker
+console.log("Welcome to the Prime Number Checker!");
+primeChecker();
