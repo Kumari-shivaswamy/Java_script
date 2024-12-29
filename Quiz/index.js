a204f6720b44b726b53c5f1f49e26dea75d945f7
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-29 19:54:06 +0530

    Update index.js

--- Quiz/index.js
+++ Quiz/index.js
@@ -1,1 +1,23 @@
+// quizGame.js
+
+function runQuiz() {
+    const questions = [
+        {
+            question: "What is the capital of France?",
+            options: ["1. Paris", "2. London", "3. Berlin", "4. Madrid"],
+            answer: 1,
+        },
+        {
+            question: "Which planet is known as the Red Planet?",
+            options: ["1. Earth", "2. Mars", "3. Jupiter", "4. Saturn"],
+            answer: 2,
+        },
+        {
+            question: "What is the square root of 64?",
+            options: ["1. 6", "2. 7", "3. 8", "4. 9"],
+            answer: 3,
+        },
+    ];
+
+   
 
