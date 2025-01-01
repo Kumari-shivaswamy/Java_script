b649894c7b66d835fd8de5cc773cf6b4328bb47f
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2025-01-01 19:32:26 +0530

    Update index.js

--- Digital_Dice_roller/index.js
+++ Digital_Dice_roller/index.js
@@ -3,6 +3,10 @@
 function rollDice(sides) {
     return Math.floor(Math.random() * sides) + 1;
 }
+function diceRollerGame() {
+    console.log("Welcome to the Digital Dice Roller!");
+    console.log("Choose a type of dice to roll:");
+   
 
 
 // Start the Digital Dice Roller
