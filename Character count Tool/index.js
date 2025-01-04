dc7508a2f0cf86004839ae0b9e5c24fe579f6c5b
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2025-01-04 12:30:03 +0530

    Update index.js

--- Character count Tool/index.js
+++ Character count Tool/index.js
@@ -4,6 +4,12 @@ function characterCounter() {
     console.log("Welcome to the Character Counter Tool!");
     const userInput = prompt("Enter a sentence or paragraph to analyze:");
 
+    if (!userInput || userInput.trim() === "") {
+        console.log("You entered an empty string. Please try again.");
+        return;
+    }
+
+   
     
 }
 
